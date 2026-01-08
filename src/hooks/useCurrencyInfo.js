ac271c8e7b4d17a51cead78082ace782c7199f14
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-01-08 16:39:06 +0530

    change API

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 const useCurrencyInfo = (currency) => {
     const [data, setData] = useState({})
     useEffect( () => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
     }, [currency])
